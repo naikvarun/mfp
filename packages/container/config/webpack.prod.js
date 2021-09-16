cae272ce7ba3c29aef8a5552d0798976bfc3596b
Varun Naik <naik.varun@gmail.com>
2021-09-16 07:21:58 -0500

    Prod build for container and marketing

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJSON = require("../package.json");
+const domain = process.env.PRODUCTiON_DOMAIN;
+
+const prodonfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJSON.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodonfig);
