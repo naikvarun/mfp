584bdce30673cd28c42f6582d6a22fa2b26fdd8e
Varun Naik <naik.varun@gmail.com>
2021-09-16 07:05:12 -0500

    First commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+const App = () => {
+  return (
+    <>
+      <h1>Container</h1>
+      <MarketingApp />
+    </>
+  );
+};
+
+export default App;
