f6b17e048a01603ce54126a4cc37267ec208741f
Varun Naik <naik.varun@gmail.com>
2021-09-21 22:12:19 -0500

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
-    <>
-      <h1>Container App from AWS</h1>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
 
