63dc147502aeb56d3b390dfe93ffab3944e57fbe
Varun Naik <naik.varun@gmail.com>
2021-09-23 06:23:30 -0500

    Added productionPrefix to avoid css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({productionPrefix: 'co'})
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
-  );
+    return (
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header/>
+                <MarketingApp/>
+            </BrowserRouter>
+        </StylesProvider>
+    );
 };
 
 export default App;
